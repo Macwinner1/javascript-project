df444bf7b77dfdd92df30d08856832fa1f6a6131
Macwinner <ifechukwuw@gmail.com>
2025-02-04 16:53:37 +0000

    update

--- smart login/index.js
+++ smart login/index.js
@@ -1,10 +1,21 @@
 
+let user = {
+    name: 'Okafor',
+    userName: "Macwinner",
+}
 let email;
 let password;
+let page;
 
 document.getElementById("log").onclick = function () {
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
-    document.getElementById("usertag").textContent = `Hello, ${email}`
+    page = document.getElementById("main").textContent 
+    document.getElementById("usertag").textContent = `Hello, ${user.userName}`
+    document.getElementById("well").textContent = `We are happy to have you on our platform...`
+    document.getElementById("reg").textContent = `Welcome`
+    function (page) {
+        if 
+    }
 }
 

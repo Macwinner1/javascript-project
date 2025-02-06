eb6a5254db988d7374d569906e85d0381ee56bbb
Macwinner <ifechukwuw@gmail.com>
2025-02-06 18:10:57 +0000

    update

--- smart login/index.js
+++ smart login/index.js
@@ -10,12 +10,9 @@ let page;
 document.getElementById("log").onclick = function () {
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
-    page = document.getElementById("main").textContent 
+    page = document.getElementById("main").textContent
     document.getElementById("usertag").textContent = `Hello, ${user.userName}`
     document.getElementById("well").textContent = `We are happy to have you on our platform...`
     document.getElementById("reg").textContent = `Welcome`
-    function (page) {
-        if 
-    }
 }
 
